dafa3247ba118616c7b4e58df34b4d72aa8a0083
matgit68 <mamat@mamat-OptiPlex-7010>
2023-09-11 16:04:55 +0200

    ex00

---
+++ ex00/Fixed.cpp
@@ -0,0 +1,30 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed() {
+	nb = 0;
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &f) {
+	std::cout << "Copy constructor called" << std::endl;
+	this->nb = f.getRawBits();
+}
+
+Fixed::~Fixed() {
+	std::cout << "Destructor called" << std::endl;
+}
+
+Fixed &Fixed::operator=(const Fixed &f) {
+	std::cout << "Copy assignment operator called" << std::endl;
+	this->nb = f.getRawBits();
+	return (*this);
+}
+
+int Fixed::getRawBits(void) const {
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->nb);
+}
+
+void Fixed::setRawBits(int const raw) {
+	this->nb = raw;
+}
---
+++ ex01/Fixed.hpp
@@ -0,0 +1,20 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+# include <iostream>
+
+class Fixed
+{
+private:
+	int	nb;
+	static const int fpart = 8;
+
+public:
+	Fixed();
+	Fixed(const Fixed &f);
+	~Fixed();
+	Fixed &operator=(const Fixed &f);
+	int getRawBits( void ) const;
+	void setRawBits( int const raw );
+};
+
+#endif
